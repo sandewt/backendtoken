057625ad68d707ff45bbfff14947ce9b3b8cfff8
jsanders <j53.sanders@gmail.com>
2023-01-12 13:22:24 +0100

    Update backendtoken.php

--- backendtoken.php
+++ backendtoken.php
@@ -12,7 +12,7 @@
  * @link      https://rolandd.com
  **/
 
-defined('_JEXEC') or die; // test
+defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Factory;
